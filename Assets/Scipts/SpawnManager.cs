d535d50a4dcd6eafbb3a8efdd98e8ac30812f62f
lichen014 <121971822+lichen014@users.noreply.github.com>
2023-02-01 11:03:07 -0800

    applied collision to objects and food

--- Assets/Scipts/SpawnManager.cs
+++ Assets/Scipts/SpawnManager.cs
@@ -7,11 +7,13 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomAnimal", 2, 1.5f);
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
