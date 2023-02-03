e01997f53a94d00cfabc65c55d4c891730c9b612
lichen014 <121971822+lichen014@users.noreply.github.com>
2023-02-03 15:41:17 -0800

    started challenge 2

--- Assets/Scipts/DestroyOutOfBounds.cs
+++ Assets/Scipts/DestroyOutOfBounds.cs
@@ -19,7 +19,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         if(transform.position.z > topBound)
         {
             Destroy(gameObject);
-        } else if (transform.position.z < lowerBound)
+        } else if (transform.position.z > lowerBound)
         {
             Debug.Log("Game Over!");
             Destroy(gameObject);
